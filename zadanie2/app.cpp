7bcfde3fb0e08cf30925981d6820c588ae682b39
Jakub Jachowski <226111@student.pwr.edu.pl>
2018-03-24 14:24:52 +0100

    trash

--- zadanie2/app.cpp
+++ zadanie2/app.cpp
@@ -13,6 +13,7 @@ const double slower = 100000000;
 const int height = 20;
 bool ready = false;
 int position_array[thread_count];
+WINDOW *windows[thread_count];
 
 struct Client{
     int x;
@@ -29,58 +30,53 @@ struct Client{
 // 	return result;
 // }
 
-// void calculate(int index){
-	
-// 	while(true){
-// 		counter+=((index+1)/slower);
-// 		//counter+=(1/slower);
+void calculate(int index){
+	while(true){
+		counter+=((index+1)/slower);
+		//counter+=(1/slower);
 
-// 		//cout<<"counter="<<counter;
-// 		if(counter>position){
-// 			//cout<<"if";
-// 			mtx.lock();
-// 			position++;
-// 			position_array[index] = position;
-// 			//cout<<"Thread"<<index<<", position="<<position_array[index]<<endl;
-// 			ready=true;
-// 			mtx.unlock();
-// 			cv.notify_all();
-// 			if(position==height) return;
-// 		}
-// 	}
-// }
-
-// void draw(){
-// 	unique_lock<mutex> lck(mtx);
-// 	while(true){
-// 		while(!ready) cv.wait(lck);
-// 		for(int i=0;i<thread_count;i++){
-// 			erase();
-// 			WINDOW *win = newwin(2,2,position_array[i]*2, i*2);
-// 			box(win, '|', '-');
-// 			touchwin(win);
-//     		wrefresh(win);
-			
-// 		}
-// 		ready=false;
-
-// 		if(position_array[0]==height){
-// 			clear();
-// 			return;
-// 		} 
-// 	}
-// }
-
-void create_client(int client_id){
-    Client client;
-    client.x=0;
-    client.y=0;
-    while(true){
-        client.x++;
-        client.y++;
-        usleep(1000000)
-    }
+		//cout<<"counter="<<counter;
+		if(counter>position){
+			//cout<<"if";
+			mtx.lock();
+			position++;
+			position_array[index] = position;
+			//cout<<"Thread"<<index<<", position="<<position_array[index]<<endl;
+			ready=true;
+			mtx.unlock();
+			cv.notify_all();
+			if(position==height) return;
+		}
+	}
 }
+
+void draw(){
+	unique_lock<mutex> lck(mtx);
+
+	while(true){
+		while(!ready) cv.wait(lck);
+		for(int i=0;i<thread_count;i++){
+			if(windows[i]==null){
+                windows[i] = newwin(2,2,i,position_array*2);
+
+            }
+            wclear(windows[i]);
+            wrefresh(windows[i]);
+            mvwin(windows[i],i,position_array*2)
+			box(windows[i], '|', '-');
+    		wrefresh(win);
+			
+		}
+		ready=false;
+
+		if(position_array[0]==height){
+			clear();
+			return;
+		} 
+	}
+}
+
+
 
 
 
@@ -89,29 +85,29 @@ int main(int argc, char *argv[])
 {
     initscr();
 
-    WINDOW *win = newwin(5, 5, 1, 1);
-    box(win, '|', '-');
-    wrefresh(win);
-    usleep(1000000);
-    wclear(win);
-    wrefresh(win);
-    usleep(1000000);
-    mvwin(win, 10, 10);
-    box(win, '|', '-');
-    wrefresh(win);
+    // WINDOW *win = newwin(5, 5, 1, 1);
+    // box(win, '|', '-');
+    // wrefresh(win);
+    // usleep(1000000);
+    // wclear(win);
+    // wrefresh(win);
+    // usleep(1000000);
+    // mvwin(win, 10, 10);
+    // box(win, '|', '-');
+    // wrefresh(win);
 
-    // thread threads[thread_count+1];
+    thread threads[thread_count+1];
 
 
-    // for(int i=0;i<thread_count;i++){
-    // 	threads[i] = thread(calculate, i);
-    // }
-    // threads[thread_count] = thread(draw);
+    for(int i=0;i<thread_count;i++){
+    	threads[i] = thread(calculate, i);
+    }
+    threads[thread_count] = thread(draw);
 
-    // for(int i=0;i<thread_count;i++){
-    // 	threads[i].join();
-    // }
-    // threads[thread_count].join();
+    for(int i=0;i<thread_count;i++){
+    	threads[i].join();
+    }
+    threads[thread_count].join();
 
     getchar();
     endwin();
