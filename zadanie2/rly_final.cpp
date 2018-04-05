f433dec8256d533de8980e02696f9a1cafa3cfcd
Jakub Jachowski <226111@student.pwr.edu.pl>
2018-04-05 12:12:04 +0200

    kinda good

--- zadanie2/rly_final.cpp
+++ zadanie2/rly_final.cpp
@@ -8,12 +8,17 @@
 #include <chrono>    
 using namespace std;
 
+const int queue_start_positionX = 7;
+const int start_height = 7;
+const int clients_count = 10;
+const int delay = 500;
+
 struct Client{
 	condition_variable cv_client;
 	int pos_x = 0;
 	int pos_y = start_height;
 	bool is_done = false;
-}
+};
 
 mutex mtx_client;
 mutex mtx_drawer;
@@ -22,16 +27,10 @@ mutex mtx_drawer;
 condition_variable cv_drawer;
 
 
-const int queue_start_positionX = 7;
-const int start_height = 7;
-const int clients_count = 10;
-const int delay = 500;
-
-
 bool drawer_busy;
 bool draw;
 
-queue<Client> waiting_clients;
+queue<Client*> waiting_clients;
 
 thread thread_array[clients_count];
 
@@ -46,15 +45,15 @@ void client(Client *client){
 	for(int i=0;i<queue_start_positionX;i++){
 		client->cv_client.wait_for(lck, chrono::milliseconds(delay));
 		client->pos_x++;
-		cout<<client->pos_x<<endl;
+		//cout<<client->pos_x<<endl;
 		if(!drawer_busy) {
 			draw = true;
-			cv_draw.notify_one();
+			cv_drawer.notify_one();
 		}
 	}
 
 	for(int i=0;i<2;i++){
-		if(waiting_clients.size==2){
+		if(waiting_clients.size()==2){
 			//do circle
 		}else{
 			waiting_clients.push(client);
@@ -72,16 +71,19 @@ void drawer(){
 		draw = false;
 		drawer_busy = true;
 		for(int i=0;i<clients_count;i++){
-			//draw clients
+			if(!client_array[i].is_done) cout<<client_array[i].pos_x<<endl;
 		}
+		drawer_busy=false;
 	}
 }
 
 int main(int argc, char *argv[])
 {
+	thread drw = thread(drawer);
 	for(int i=0;i<clients_count;i++){
 		thread_array[i] = thread(client, &client_array[i]);
 	}
+	
     
     getchar();
     return 0;
