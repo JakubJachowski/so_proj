79bbcb85359c5e8e1a0cd94a55895cabb5785bfa
Jakub Jachowski <226111@student.pwr.edu.pl>
2018-04-09 18:11:47 +0200

    almost done

--- zadanie2/rly_final.cpp
+++ zadanie2/rly_final.cpp
@@ -14,14 +14,16 @@ using namespace std;
 const int queue_start_positionX = 7;
 const int start_height = 10;
 const int clients_count = 100;
-const int delay = 100;
-const int circleSize = 5;
+const int delay = 300;
+const int circleSize = 6;
 
 struct Client{
 	condition_variable cv_client;
 	int pos_x = 0;
 	int pos_y = start_height;
 	bool is_done = false;
+	bool in_queue = false;
+	bool being_served = false;
 	int color = 0;
 };
 
@@ -99,7 +101,17 @@ void client(Client *client){
 
 	for(int i=0;i<2;i++){
 		if(waiting_clients.size()==2){
-			if(doing_circle_clients.size()==2) break;
+			if(doing_circle_clients.size()==2) {
+				for(int i=0;i<50;i++){
+					client->cv_client.wait_for(lck, chrono::milliseconds(delay));
+					client->pos_y--;
+					if(!drawer_busy) {
+						draw = true;
+						cv_drawer.notify_one();
+					}
+				}
+				return;	
+			}
 		//if(true){
 			doing_circle_clients.push(client);
 			while(!isCircleFinished(client)){
@@ -114,6 +126,11 @@ void client(Client *client){
 			doing_circle_clients.pop();
 		}else{
 			waiting_clients.push(client);
+			client->in_queue = true;
+			if(!drawer_busy) {
+				draw = true;
+				cv_drawer.notify_one();
+			}
 			while(!client->is_done) client->cv_client.wait(lck);
 			finished_clients.push(client);
 			break;
@@ -133,6 +150,15 @@ void client(Client *client){
 
 void drawer(){
 	unique_lock<mutex> lck(mtx_drawer);
+
+	string tmp_queue_status;
+	string tmp_circle_status;
+	string tmp_finished;
+					
+	const char *queue_status;
+	const char *circle_status;
+	const char *finished_clients_status;
+
 	while(true){
 		while(!draw) cv_drawer.wait(lck);
 		//cout<<"im drawing";
@@ -140,28 +166,50 @@ void drawer(){
 		drawer_busy = true;
 		clear();
 		for(int i=0;i<clients_count;i++){
-			if(!client_array[i].is_done){
+			if(!client_array[i].in_queue && !client_array[i].being_served){
 				//cout<<client_array[i].pos_x<<" "<<client_array[i].pos_y<<endl;
 				attron(COLOR_PAIR(client_array[i].color));
-				mvprintw(client_array[i].pos_y,client_array[i].pos_x,"x");
-				string tmp_queue_status = "Queue count: " + to_string(waiting_clients.size());
-				string tmp_circle_status = "Circle count: " + to_string(doing_circle_clients.size());
-				string tmp_finished = "Finished count: " + to_string(finished_clients.size());
-				
-				
-				const char *queue_status = tmp_queue_status.c_str();
-				const char *circle_status = tmp_circle_status.c_str();
-				const char *finished_clients_status = tmp_finished.c_str();
-				
-				attron(COLOR_PAIR(1));
-				mvprintw(0,0,queue_status);
-				mvprintw(1,0,circle_status);
-				mvprintw(2,0,finished_clients_status);				
-								
-				refresh();
-			}else{
-				//cout<<"dude is done"<<endl;
+				mvprintw(client_array[i].pos_y,client_array[i].pos_x,"x");								
 			}
+			if(client_array[i].being_served){
+				attron(COLOR_PAIR(client_array[i].color));
+				mvprintw(start_height, queue_start_positionX + 2*circleSize- 1, "x");
+			}
+			///drawing status
+			tmp_queue_status = "Queue count: " + to_string(waiting_clients.size());
+			tmp_circle_status = "Circle count: " + to_string(doing_circle_clients.size());
+			tmp_finished = "Finished count: " + to_string(finished_clients.size());
+			
+			
+			queue_status = tmp_queue_status.c_str();
+			circle_status = tmp_circle_status.c_str();
+			finished_clients_status = tmp_finished.c_str();
+			
+			attron(COLOR_PAIR(1));
+			mvprintw(0,0,queue_status);
+			mvprintw(1,0,circle_status);
+			mvprintw(2,0,finished_clients_status);
+			mvprintw(start_height-1, queue_start_positionX + 1, "Queue");
+			mvprintw(start_height-1, queue_start_positionX + 2*circleSize- 1, "K");
+			
+
+			//drawing queue
+			if(waiting_clients.size()==2){
+				attron(COLOR_PAIR(waiting_clients.back()->color));
+				mvprintw(start_height, queue_start_positionX + 1, "x");
+				attron(COLOR_PAIR(waiting_clients.front()->color));
+				mvprintw(start_height, queue_start_positionX + 2, "x");
+			}else{
+				if(waiting_clients.front()!=NULL){
+					attron(COLOR_PAIR(waiting_clients.front()->color));
+					mvprintw(start_height, queue_start_positionX + 2, "x");
+				}
+			}
+			refresh();
+			// if(waiting_clients.back()!=NULL){
+			// 	attron(COLOR_PAIR(waiting_clients.back()->color));
+			// 	mvprintw(start_height, queue_start_positionX + 1, "x");
+			// }
 		}
 		drawer_busy=false;
 	}
@@ -169,12 +217,25 @@ void drawer(){
 
 void cashier(){
 	unique_lock<mutex> lck(mtx_cashier);
+	Client *temp;
 	while(true){
-		cv_cashier.wait_for(lck, chrono::milliseconds(delay*10));
 		if(waiting_clients.size()!=0) {
-			waiting_clients.front()->is_done=true;
-			waiting_clients.front()->cv_client.notify_one();
+			temp = waiting_clients.front();
 			waiting_clients.pop();
+			temp->being_served=true;			
+			temp->in_queue=false;
+			if(!drawer_busy) {
+				draw = true;
+				cv_drawer.notify_one();
+			}
+			cv_cashier.wait_for(lck, chrono::milliseconds(delay*10));
+			temp->is_done=true;
+			temp->being_served=false;						
+			waiting_clients.front()->cv_client.notify_one();			
+			if(!drawer_busy) {
+				draw = true;
+				cv_drawer.notify_one();
+			}
 		}
 	}
 	
@@ -189,6 +250,7 @@ int main(int argc, char *argv[])
 	
 	initscr();
 	start_color();
+	curs_set(0);
 	init_pair(1, COLOR_BLACK, COLOR_CYAN);
 	init_pair(2, COLOR_CYAN, COLOR_CYAN);
 	init_pair(3, COLOR_BLUE, COLOR_BLUE);
